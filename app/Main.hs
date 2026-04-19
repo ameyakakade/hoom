80769e05d8847e494b25ab1de71d98c9bb27caa9
ameyakakade <ameyakakade@gmail.com>
2026-04-19 17:25:16 +0530

    Added new texture

--- app/Main.hs
+++ app/Main.hs
@@ -28,7 +28,7 @@ import ParseLevel
 startup :: IO AppState 
 startup = do 
   window <- initWindow width height "hoom"
-  let texturePaths = ["textures/dummy.png", "textures/wall1.png", "textures/wall2.png", "textures/wall3.png", "textures/wall4.png", "textures/error.png"]
+  let texturePaths = ["textures/floor_01.png", "textures/wall1.png", "textures/wall2.png", "textures/wall3.png", "textures/wall4.png", "textures/error.png", "textures/sky.png"]
   loadedTextures <- sequence $ map loadTexture texturePaths
   disableCursor
 
